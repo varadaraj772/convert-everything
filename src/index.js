81c9b0214b282bc76010c04a54d42d898f561b44
varadaraj772 <varadaraj277@gmail.com>
2024-12-11 17:54:19 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 export { length } from "./converters/length.js";
 export { area } from "./converters/area.js";
-export { volume, VolumeMeasures, convertVolume } from "./converters/volume.js";
+export { volume } from "./converters/volume.js";
 export { time } from "./converters/time.js";
 export { energy } from "./converters/energy.js";
 export { temperature } from "./converters/temperature.js";
 export { pressure } from "./converters/pressure.js";
 export { data } from "./converters/data.js";
-export {weight} from "./converters/weight.js";
+export { weight } from "./converters/weight.js";
